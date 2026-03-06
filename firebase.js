54af8f764b2dc4fccfbf267341ab4a70fea6460c
grahaalanajadimulyablokdepan <grahaalanajadimulya@gmail.com>
2026-03-07 02:18:46 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,12 @@
-const firebaseConfig = { apiKey: "API_KEY", authDomain: "PROJECT.firebaseapp.com", projectId: "PROJECT_ID", storageBucket: "PROJECT.appspot.com", messagingSenderId: "123456", appId: "APP_ID" }
+const firebaseConfig = {
+  apiKey: "AIzaSyDjT6Xr2lZ8SWxD_ZY3abBIJbn_713C1bU",
+  authDomain: "kas-blok-depan-grahaalana.firebaseapp.com",
+  projectId: "kas-blok-depan-grahaalana",
+  storageBucket: "kas-blok-depan-grahaalana.firebasestorage.app",
+  messagingSenderId: "878971761602",
+  appId: "1:878971761602:web:2655c0986f67daeb5dc5b"
+};
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore()
+const db = firebase.firestore();
