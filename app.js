a773cdd8edf570be07ec95d53ea24ef0f480da4d
grahaalanajadimulyablokdepan <grahaalanajadimulya@gmail.com>
2026-03-07 22:40:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* PASSWORD LOGIN */
 
-const passwords = [
+const passwords=[
 "ketua123",
 "sekretaris123",
 "bendahara123",
@@ -11,12 +11,12 @@ const passwords = [
 
 function loginAdmin(){
 
-let pass = document.getElementById("adminPass").value
+let pass=document.getElementById("adminPass").value
 
 if(passwords.includes(pass)){
 
-document.getElementById("loginBox").style.display = "none"
-document.getElementById("adminPanel").style.display = "block"
+document.getElementById("loginBox").style.display="none"
+document.getElementById("adminPanel").style.display="block"
 
 }else{
 
@@ -27,74 +27,17 @@ alert("Password salah")
 }
 
 
-/* FIREBASE */
-
-const db = firebase.firestore()
-
-
-
-/* DATA BLOK */
-
-const blokData = {
-"A1":20,
-"A2":24,
-"A3":10,
-"B1":20,
-"B2":20,
-"B3":20
-}
-
-
-
-/* GENERATE PETA RUMAH */
-
-function generateMap(){
-
-let html=""
-
-for(let blok in blokData){
-
-html+=`
-<div class="blok">
-<div class="blok-title">Blok ${blok}</div>
-<div class="rumah-grid">
-`
-
-for(let i=1;i<=blokData[blok];i++){
-
-html+=`
-<div class="rumah belum"
-onclick="lihatRumah('${blok}',${i})">
-
-${blok}-${i}
-
-</div>
-`
-
-}
-
-html+=`</div></div>`
-
-}
-
-document.getElementById("mapPerumahan").innerHTML = html
-
-}
-
-generateMap()
-
-
 
 /* TAMBAH IURAN */
 
 function tambahIuran(){
 
-let nama = document.getElementById("nama").value
-let blok = document.getElementById("blok").value
-let rumah = document.getElementById("rumah").value
-let bulan = document.getElementById("bulan").value
-let tahun = document.getElementById("tahun").value
-let jumlah = document.getElementById("jumlah").value
+let nama=document.getElementById("nama").value
+let blok=document.getElementById("blok").value
+let rumah=document.getElementById("rumah").value
+let bulan=document.getElementById("bulan").value
+let tahun=document.getElementById("tahun").value
+let jumlah=document.getElementById("jumlah").value
 
 db.collection("iuran").add({
 
@@ -110,6 +53,7 @@ jumlah:Number(jumlah)
 alert("Iuran berhasil disimpan")
 
 loadIuran()
+loadDashboard()
 
 }
 
@@ -119,8 +63,8 @@ loadIuran()
 
 function tambahPengeluaran(){
 
-let ket = document.getElementById("ket").value
-let jumlah = document.getElementById("jumlahKeluar").value
+let ket=document.getElementById("ket").value
+let jumlah=document.getElementById("jumlahKeluar").value
 
 db.collection("pengeluaran").add({
 
@@ -129,7 +73,7 @@ jumlah:Number(jumlah)
 
 })
 
-alert("Pengeluaran disimpan")
+alert("Pengeluaran berhasil disimpan")
 
 loadDashboard()
 
@@ -137,17 +81,17 @@ loadDashboard()
 
 
 
-/* LOAD RIWAYAT IURAN */
+/* LOAD RIWAYAT */
 
 function loadIuran(){
 
-db.collection("iuran").get().then((snapshot)=>{
+db.collection("iuran").get().then(snapshot=>{
 
 let html=""
 
 snapshot.forEach(doc=>{
 
-let d = doc.data()
+let d=doc.data()
 
 html+=`
 <tr>
@@ -162,7 +106,7 @@ html+=`
 
 })
 
-document.getElementById("tabelIuran").innerHTML = html
+document.getElementById("tabelIuran").innerHTML=html
 
 })
 
@@ -176,36 +120,31 @@ loadIuran()
 
 function loadDashboard(){
 
-let totalIuran = 0
-let totalKeluar = 0
+let totalIuran=0
+let totalKeluar=0
 
-
-db.collection("iuran").get().then((snapshot)=>{
-
-snapshot.forEach(doc=>{
-
-totalIuran += doc.data().jumlah
-
-})
-
-document.getElementById("totalIuran").innerText = "Rp"+totalIuran
-
-})
-
-
-db.collection("pengeluaran").get().then((snapshot)=>{
+db.collection("iuran").get().then(snapshot=>{
 
 snapshot.forEach(doc=>{
+totalIuran+=doc.data().jumlah
+})
 
-totalKeluar += doc.data().jumlah
+document.getElementById("totalIuran").innerText="Rp"+totalIuran
 
 })
 
-document.getElementById("totalKeluar").innerText = "Rp"+totalKeluar
 
-let totalKas = totalIuran - totalKeluar
+db.collection("pengeluaran").get().then(snapshot=>{
 
-document.getElementById("totalKas").innerText = "Rp"+totalKas
+snapshot.forEach(doc=>{
+totalKeluar+=doc.data().jumlah
+})
+
+document.getElementById("totalKeluar").innerText="Rp"+totalKeluar
+
+let kas=totalIuran-totalKeluar
+
+document.getElementById("totalKas").innerText="Rp"+kas
 
 })
 
@@ -215,83 +154,14 @@ loadDashboard()
 
 
 
-/* CEK RUMAH */
-
-function cekRumah(){
-
-let blok = document.getElementById("cekBlok").value
-let nomor = document.getElementById("cekNomor").value
-
-db.collection("iuran")
-.where("blok","==",blok)
-.where("rumah","==",nomor)
-.get()
-.then(snapshot=>{
-
-if(snapshot.empty){
-
-document.getElementById("hasil").innerHTML =
-"Belum ada pembayaran"
-
-}else{
-
-let html=""
-
-snapshot.forEach(doc=>{
-
-let d = doc.data()
-
-html += `
-<div>
-${d.bulan} ${d.tahun} : Rp${d.jumlah}
-</div>
-`
-
-})
-
-document.getElementById("hasil").innerHTML = html
-
-}
-
-})
-
-}
-
-
-
-/* NOMOR RUMAH */
-
-for(let i=1;i<=25;i++){
-
-let opt=document.createElement("option")
-
-opt.value=i
-opt.text=i
-
-document.getElementById("cekNomor").appendChild(opt)
-
-}
-
-
-
 /* EXPORT EXCEL */
 
 function exportExcel(){
 
-let table = document.querySelector("table")
+let table=document.querySelector("table")
 
-let wb = XLSX.utils.table_to_book(table)
+let wb=XLSX.utils.table_to_book(table)
 
 XLSX.writeFile(wb,"laporan-kas.xlsx")
-
-}
-
-
-
-/* KLIK RUMAH */
-
-function lihatRumah(blok,no){
-
-alert("Rumah "+blok+"-"+no)
 
 }
